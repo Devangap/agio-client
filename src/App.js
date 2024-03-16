506aa7e2434a2b41108aca462b711e767bdf1db8
Devangap <101203575+Devangap@users.noreply.github.com>
2024-03-16 13:13:21 +0530

    commit3

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import { Toaster } from 'react-hot-toast';
 import Home from './pages/Home';
+import Main_login from './pages/Main_login';
 
 
 
@@ -15,6 +16,7 @@ function App() {
       <Toaster position='top-center' reverseOrder={false} />
         <Routes>
           <Route path='/Main_Register' element={<Main_register />} />
+          <Route path='/Main_Login' element={<Main_login />} />
           <Route path='/login' element={<Login />} />
           <Route path='/register' element={< Register/>} />
           <Route path='/home' element={< Home/>} />
