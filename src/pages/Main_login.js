6fdbb66e8f0c225f1219b1c1594bbe13e7eaa788
Devangap <101203575+Devangap@users.noreply.github.com>
2024-03-16 15:10:19 +0530

    loginpage - ui

--- src/pages/Main_login.js
+++ src/pages/Main_login.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Button, Form ,  Input,Select, DatePicker } from 'antd'
-import '../mainreg.css';
+import '../mainlogin.css';
+import loginImage from '/Users/dev/Documents/GitHub/agio-client/src/Images/login.jpg';
 
 
 
@@ -14,78 +15,34 @@ function Main_login() {
   }
   
   return (
-    <div className="mainreg">
-  <div className="main_login_form box p-3">
-    <h3 className='title'>Login</h3>
+    <div className="mainlog">
+  <div className="main_login_form_log box_log p-3">
+  <div className="login_image">
+          <img src={loginImage} alt="Login Image" />
+        </div>
+        
+    <h3 className='title_log'>Login</h3>
     <Form layout='vertical' onFinish={onFinish}>
-<<<<<<< Updated upstream
-      <div className="form-row">
-        <div className="item">
-          <Form.Item label='Full Name' name='name'>
-            <Input placeholder='Full name' />
-          </Form.Item>
-        </div>
-        <div className="item">
-          <Form.Item label="Date of Birth" name="dob">
-            <DatePicker className="date" />
-          </Form.Item>
-        </div>
-      </div>
-=======
-      
->>>>>>> Stashed changes
-      <div className="form-row">
-        <div className="item">
+    <div className="content">
+   
+     
+      <div className = 'login'>
+      <div className = 'field'>
           <Form.Item label='Username' name='username'>
             <Input placeholder='Username' />
           </Form.Item>
-        </div>
-        <div className="item">
+          </div>
+         
           <Form.Item label='Password' name='password'>
             <Input placeholder='Password' />
           </Form.Item>
-        </div>
+          
+          </div>
+      
+      <div className="Button-conslog">
+        <Button className='primary-button my-2' htmlType='submit'>LOGIN</Button>
       </div>
-<<<<<<< Updated upstream
-      <div className="form-row">
-        <div className="item">
-          <Form.Item label='Job Title' name='jobRole'>
-            <Input placeholder='Job Title' />
-          </Form.Item>
-        </div>
-        <div className="item">
-          <Form.Item name="shift" label="Shift">
-            <Select className="shift" placeholder="Select a shift">
-              <Option value="morning">08:00 - 14:00</Option>
-              <Option value="evening">14:00 - 20:00</Option>
-              <Option value="other">No specific shift</Option>
-            </Select>
-          </Form.Item>
-        </div>
       </div>
-      <div className="form-row">
-        <div className="item">
-          <Form.Item label="Date Joined" name="dateJoined">
-            <DatePicker className="date" />
-          </Form.Item>
-        </div>
-        <div className="item">
-          <Form.Item label='Phone Number' name='phoneNumber'>
-            <Input placeholder='Phone Number' />
-          </Form.Item>
-        </div>
-      </div>
-      <div className="item">
-        <Form.Item name="address" label="Address">
-          <Input.TextArea className='address' />
-        </Form.Item>
-      </div>
-      <div className="Button-cons">
-        <Button className='primary-button my-2' htmlType='submit'>REGISTER</Button>
-      </div>
-=======
-    
->>>>>>> Stashed changes
     </Form>
   </div>
 </div>
