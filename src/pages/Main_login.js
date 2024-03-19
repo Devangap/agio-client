214909b4d3808ae65dfb5035c325ecdecd18c339
Devangap <101203575+Devangap@users.noreply.github.com>
2024-03-19 12:08:57 +0530

    sdsdsd

--- src/pages/Main_login.js
+++ src/pages/Main_login.js
@@ -37,19 +37,19 @@ function Main_login() {
   
   
   return (
-    <div className="mainlog">
-  <div className="main_login_form_log box_log p-3">
-  <div className="login_image">
+    <div className="Mmainlog">
+  <div className="Mmain_login_form_log box_log p-3">
+  <div className="Mlogin_image">
           <img src={loginImage} alt="Login Image" />
         </div>
         
-    <h3 className='title_log'>Login</h3>
+    <h3 className='Mtitle_log'>Login</h3>
     <Form layout='vertical' onFinish={onFinish}>
-    <div className="content">
+    <div className="Mcontent">
    
      
-      <div className = 'login'>
-      <div className = 'field'>
+      <div className = 'Mlogin'>
+      <div className = 'Mfield'>
           <Form.Item label='Username' name='username_log'>
             <Input placeholder='Username' />
           </Form.Item>
@@ -61,8 +61,8 @@ function Main_login() {
           
           </div>
       
-      <div className="Button-conslog">
-        <Button className='primary-button my-2' htmlType='submit'>LOGIN</Button>
+      <div className="MButton-conslog">
+        <Button className='Mprimary-button my-2' htmlType='submit'>LOGIN</Button>
       </div>
       </div>
     </Form>
