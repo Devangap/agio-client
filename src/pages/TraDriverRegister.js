9698899798d9acd8b33c5bde69e17d9f39015fb4
shenal chanuka <125878126+shenalchanu8@users.noreply.github.com>
2024-03-23 22:48:46 +0530

    add234

--- src/pages/TraDriverRegister.js
+++ src/pages/TraDriverRegister.js
@@ -1,22 +1,24 @@
 import React from 'react'
+import axios from 'axios';
 import {Button, Form ,  Input,Select, DatePicker } from 'antd'
-import '../mainreg.css';
-import axios from "axios";
+import Layout from '../components/Layout';
 import toast from 'react-hot-toast';
-import TraLayout from '../components/TraLayout'
-
-
-
+import { useNavigate } from 'react-router-dom';
 
 function TraDriverRegister() {
-  const { Option } = Select;
 
-  const onFinish = async(values) => {
+  const navigate = useNavigate();
+
+  const{Option} = Select;
+
+  const onFinish = async(values) =>{
     console.log('Recieved values of form', values);
+
     try {
-      const response = await axios.post('/api/employee/Main_register', values);
+      const response = await axios.post('/api/TransportRoute/TraBooking', values);
       if(response.data.success){
           toast.success(response.data.message);
+          navigate('/TraBookingDisplay');
          
           
       }else{
@@ -27,77 +29,58 @@ function TraDriverRegister() {
   } catch (error) {
       toast.error("Something went wrong");
   }
-     
-  };
-  return <TraLayout>
-    <div className="mainreg">
-  <div className="main_login_form box p-3">
-    <h3 className='title'>Employee Registration Form</h3>
+
+  }
+
+  return <Layout>
+    <div className="annform">
+  <div className="AnnHRSup_form box p-3">
+    <h3 className='title'>CREATE DRIVER ACCOUNT</h3>
     <Form layout='vertical' onFinish={onFinish}>
       <div className="form-row">
-        <div className="item">
-          <Form.Item label='Full Name' name='fname'>
-            <Input placeholder='Full name' />
+      <div className="item">
+          <Form.Item label='Driver Name' name='driName'>
+            <Input placeholder='Driver Name' />
           </Form.Item>
         </div>
-        <div className="item">
-          <Form.Item label="Date of Birth" name="dob">
-            <DatePicker className="date" />
-          </Form.Item>
-        </div>
+        
       </div>
+      
       <div className="form-row">
-        <div className="item">
-          <Form.Item label='Username' name='username_log'>
-            <Input placeholder='Username' />
+      <div className="item">
+      <Form.Item label='Driver Email' name='driEmail'>
+            <Input placeholder='Driver Email' />
           </Form.Item>
         </div>
         <div className="item">
-          <Form.Item label='Password' name='password_log'>
-            <Input placeholder='Password' />
-          </Form.Item>
-        </div>
-      </div>
-      <div className="form-row">
-        <div className="item">
-          <Form.Item label='Job Title' name='jobRole'>
-            <Input placeholder='Job Title' />
-          </Form.Item>
-        </div>
-        <div className="item">
-          <Form.Item name="shift" label="Shift">
-            <Select className="shift" placeholder="Select a shift">
-              <Option value="morning">08:00 - 14:00</Option>
-              <Option value="evening">14:00 - 20:00</Option>
-              <Option value="other">No specific shift</Option>
+          <Form.Item name="TypeWork Expereance" label="Work Expereance">
+            <Select className="Type" placeholder="Select Work Expereance">
+              <Option value="General">0-5 years</Option>
+              <Option value="Specific">6-10 years</Option>
+              <Option value="Specific">above 10 years</Option>
             </Select>
           </Form.Item>
         </div>
       </div>
       <div className="form-row">
         <div className="item">
-          <Form.Item label="Date Joined" name="dateJoined">
+          <Form.Item label="select Register Date" name="regdate">
             <DatePicker className="date" />
-          </Form.Item>
-        </div>
-        <div className="item">
-          <Form.Item label='Phone Number' name='phoneNumber'>
-            <Input placeholder='Phone Number' />
           </Form.Item>
         </div>
       </div>
       <div className="item">
-        <Form.Item name="address" label="Address">
-          <Input.TextArea className='address' />
-        </Form.Item>
+      <Form.Item label='Driver PhoneNumber' name='driPnum'>
+            <Input placeholder='Phone Number' />
+          </Form.Item>
       </div>
       <div className="Button-cons">
-        <Button className='primary-button my-2' htmlType='submit'>REGISTER</Button>
+        <Button className='primary-button my-2' htmlType='submit'>Submit</Button>
       </div>
     </Form>
   </div>
 </div>
-  </TraLayout>
+  </Layout>
 }
 
 export default TraDriverRegister
