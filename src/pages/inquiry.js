7a6da3315324efd6ff6e33dd3f6d799b28d9c60c
IT22065544 <mmweerakoon119@gmail.com>
2024-03-23 11:08:14 +0530

    code

--- src/pages/inquiry.js
+++ src/pages/inquiry.js
@@ -1,59 +1,56 @@
 import React from 'react';
-import { Button, Form, Input, Select, DatePicker, Upload } from 'antd';
+import { Button, Form, Input, DatePicker } from 'antd';
 import '../inquiry.css';
-import { UploadOutlined } from '@ant-design/icons';
-import Image from 'antd';
+import Layout from './inquiryLayout';
+import axios from "axios";
 
 function Inquiry() {
-  const { Option } = Select;
 
-  const onFinish = values => {
+  const onFinish = async (values) => {
     console.log('Received values of form:', values);
+    try {
+      const response = await axios.post('/api/inquiry/inquiry', values);
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.error('Error submitting inquiry:', error);
+    }
   };
-  
+
   return (
-    <div className="inquiry-container">
-
-      <div className="image-container">
-
-      </div>
-      <div className="iform-container">
-        <div className="imain_form box p-3">
-          <h3 className='ititle'>Enter Inquiry Form</h3>
-          <Form layout='horizontal' onFinish={onFinish}>
-            <div className="iform-row">
-              <div className="iitem">
-                <Form.Item label='Full Name' name='name'>
-                  <Input placeholder='Full name' />
-                </Form.Item>
-                <Form.Item label='Username' name='username'>
-                  <Input placeholder='Username' />
-                </Form.Item>
-                <Form.Item label="Pick a date" name="inquirydate">
-                  <DatePicker className="date" />
-                </Form.Item>
-                <Form.Item label='Phone Number' name='phoneNumber'>
-                  <Input placeholder='Phone Number' />
-                </Form.Item>
-                <Form.Item name="describe" label="Enter Inquiry">
-                  <Input.TextArea className='idescribe' />
-                </Form.Item>
-                <Form.Item label='Attach Document'>
-                  <Form.Item name="attachment" valuePropName="fileList" noStyle>
-                    <Upload name="logo" action="/upload.do" listType="text">
-                      <Button icon={<UploadOutlined />}>Click to Upload</Button>
-                    </Upload>
+    <Layout>
+      <div className="inquiry-container">
+        <div className="image-container"></div>
+        <div className="iform-container">
+          <div className="imain_form box p-3">
+            <h3 className='ititle'>Enter Inquiry Form</h3>
+            <Form layout='horizontal' onFinish={onFinish}>
+              <div className="iform-row">
+                <div className="iitem">
+                  <Form.Item label='Full Name' name='name' rules={[{ required: true, message: 'Please input your full name!' }]}>
+                    <Input placeholder='Full name' />
                   </Form.Item>
-                </Form.Item>
+                  <Form.Item label='Username' name='username' rules={[{ required: true, message: 'Please input your username!' }]}>
+                    <Input placeholder='Username' />
+                  </Form.Item>
+                  <Form.Item label="Pick a date" name="inquirydate" rules={[{ required: true, message: 'Please select inquiry date!' }]}>
+                    <DatePicker className="date" />
+                  </Form.Item>
+                  <Form.Item label='Phone Number' name='phoneNumber' rules={[{ required: true, message: 'Please input your phone number!' }]}>
+                    <Input placeholder='Phone Number' />
+                  </Form.Item>
+                  <Form.Item name="describe" label="Enter Inquiry" rules={[{ required: true, message: 'Please enter your inquiry!' }]}>
+                    <Input.TextArea className='idescribe' />
+                  </Form.Item>
+                </div>
               </div>
-            </div>
-            <div className="iButton-cons">
-              <Button className='iprimary-button my-2' htmlType='submit'>SUBMIT</Button>
-            </div>
-          </Form>
+              <div className="iButton-cons">
+                <Button className='iprimary-button my-2' htmlType='submit'>SUBMIT</Button>
+              </div>
+            </Form>
+          </div>
         </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
