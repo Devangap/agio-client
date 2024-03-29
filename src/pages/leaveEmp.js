e7820b615e514e3a4e14b71565ba0ba2e8b9f4db
Devangap <101203575+Devangap@users.noreply.github.com>
2024-03-29 22:52:31 +0530

    leave form

--- src/pages/leaveEmp.js
+++ src/pages/leaveEmp.js
@@ -1,39 +1,95 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Table, Button, message, Modal, Form, Input, DatePicker } from 'antd';
 import Layout from '../components/Layout'
 import axios from 'axios';
 import '../leaveEmp.css';
 import { useNavigate } from 'react-router-dom';
+import { UseSelector } from 'react-redux';
 
 function LeaveEmp() {
+    const [leaveData, setLeaveData] = useState([]);
     const navigate = useNavigate();
-    const getData = async () => {
+   
+
+
+    const fetchData = async () => {
         try {
-            const response = await axios.post('/api/employee/get-employee-info-by-id', {} , {
+            const response = await axios.get('/api/leave/getleave', {
                 headers: {
                     Authorization: 'Bearer ' + localStorage.getItem('token')
                 },
             });
-            console.log(response.data);
+            setLeaveData(response.data.leave); // Assuming response.data.leave is an array of objects
         } catch (error) {
             console.log(error);
         }
     };
 
     useEffect(() => {
-        getData();
+        fetchData();
     }, []);
 
     const handleLeaveSubmission = () => {
-        navigate('/leaveEmpform'); 
+        navigate('/leaveEmpform');
+    };
+    const handleDelete = async (id) => {
+        try {
+            await axios.delete(`/api/annWorkouts/deleteAnnHRsup/${id}`);
+            setLeaveData(prev => prev.filter(item => item._id !== id));
+            message.success('Announcement deleted successfully');
+        } catch (error) {
+            message.error('Failed to delete announcement');
+        }
     };
 
-  return <Layout>
-    <div>
-    <button className="leavesub" onClick={handleLeaveSubmission}>Leave Submission</button>
-    </div>
-  
-  </Layout>
+        const columns = [
+            {
+                title: 'Name',
+                dataIndex: 'name',
+                key: 'name',
+            },
+            {
+                title: 'RangePicker',
+                dataIndex: 'RangePicker',
+                key: 'RangePicker',
+            },
+            {
+                title: 'Type',
+                dataIndex: 'Type',
+                key: 'Type',
+            },
+            {
+                title: 'Department',
+                dataIndex: 'department',
+                key: 'department',
+            },
+            {
+                title: 'Description',
+                dataIndex: 'Description',
+                key: 'Description',
+            },
+        
+        {
+            title: 'Action',
+            key: 'action',
+            render: (_, record) => (
+                <>
+                    <Button type="primary" className="update" onClick={() => navigate(`/leaveUpdate/${record._id}`)}>Update</Button>
+                    <Button danger onClick={() => handleDelete(record._id)}>Delete</Button>
+                </>
+            ),
+        },
+        // Define your table columns here
+    ];
+
+    return (
+        <Layout>
+            <div>
+                <button className="leavesub" onClick={handleLeaveSubmission}>Leave Submission</button>
+            </div>
+            <Table dataSource={leaveData} columns={columns} />
+        </Layout>
+    );
 }
 
-
-export default LeaveEmp
+export default LeaveEmp;
--- src/pages/leaveEmpform.js
+++ src/pages/leaveEmpform.js
@@ -1,103 +1,99 @@
-import React from 'react'
-import {Button, Form ,  Input,Select, DatePicker,message,Upload } from 'antd'
-import "../leaveEmpform.css"
-
+import React, { useEffect } from 'react';
+import { Button, Form, Input, Select, DatePicker, Upload, message } from 'antd';
+import "../leaveEmpform.css";
 import axios from "axios";
 import toast from 'react-hot-toast';
+import { useNavigate } from 'react-router';
+import { useState } from 'react';
+import { useDispatch ,useSelector} from 'react-redux';
+import { showLoading ,hideLoading} from '../redux/empalerts';
 
 function LeaveEmpform() {
     const { RangePicker } = DatePicker;
     const { Option } = Select;
+    const [userData, setUserData] = useState({}); 
 
-    const normFile = (e) => {
-        console.log('Upload event:', e);
-        if (Array.isArray(e)) {
-          return e;
-        }
-        return e?.fileList;
-      };
-
-  const onFinish = async(values) =>{
-    console.log('Recieved values of form', values);
-    try {
-        const response = await axios.post('/api/leave/leaveEmpform', values);
-        if(response.data.success){
-            toast.success(response.data.message);
-           
-            
-        }else{
-            toast.error(response.data.message);
-  
-        }
-        
-    } catch (error) {
-        toast.error("Something went wrong");
-    }
-       
-
-
-  };
-
-
-  
-
-
-
-  return (
-  <div className="leaveform">
-  <div className="leave_formbox p-3">
-    <h3 className='leave_title'>Leave Submission Form</h3>
-    <Form layout='vertical' onFinish={onFinish}>
-      <div className="leave_form-row">
-      <div className="leave_item">
-          <Form.Item label='Employee Name' name='name'>
-            <Input placeholder='Employee name' />
-          </Form.Item>
-          <Form.Item label='Department' name='department'>
-            <Input placeholder='Department' />
-          </Form.Item>
-        </div>
-        
-      </div>
+    const navigate = useNavigate();
+    const {user} = useSelector((state) => state.user);
+   const dispatch = useDispatch();
+    const onFinish = async (values) => {
+        console.log('Received values of form', values);
+        try {
+            dispatch(showLoading());
+            const response = await axios.post('/api/employee/Main_register', {...values , userId : user._id,});
+            dispatch(hideLoading());
+            if(response.data.success){
+                navigate("/leaveEmp")
+               
+                
+            }else{
+                toast.error(response.data.message);
       
-      <div className="leave_form-row">
-      <div className="leave_item">
-      <Form.Item
-      label="Leave Duration"
-      name="RangePicker"
-      rules={[{ required: false, message: 'Please input!' }]}
-    >
-      <RangePicker  /> 
-    </Form.Item>
-        </div>
-       
-      </div>
-      <div className="leave_form-row">
-        <div className="leave_item">
-        <div className="leave_item">
-          <Form.Item name="Type" label="Select leave type">
-            <Select className="leave_Type" placeholder="Select leave type">
-              <Option value="General">General</Option>
-              <Option value="Specific">Annual </Option>
-              <Option value="Medical">Medical </Option>
-            </Select>
-          </Form.Item>
-        </div>
-       
-            </div>
-      </div>
-      <div className="leave_item">
-        <Form.Item name="Description" label="Description">
-          <Input.TextArea className='leave_Description' />
-        </Form.Item>
-      </div>
-      <div className="leave_Button-cons">
-        <Button className='leave_primary-button my-2' htmlType='submit'>Submit</Button>
-      </div>
-    </Form>
-  </div>
-</div>
-  )
-} 
+            }
+            
+        } catch (error) {
+          dispatch(hideLoading());
+            toast.error("Something went wrong");
+        }
+    };
 
-export default LeaveEmpform 
+    return (
+        <div className="leaveform">
+            <div className="leave_formbox p-3">
+                <h3 className='leave_title'>Leave Submission Form</h3>
+                <Form layout='vertical' onFinish={onFinish}>
+                    <div className="leave_form-row">
+                        <div className="leave_item">
+                            <Form.Item label='Employee Name' name='name'>
+                                <Input placeholder='Employee name' />
+                            </Form.Item>
+                        </div>
+                        <div className="leave_item">
+                            <Form.Item label='Department' name='department'>
+                                <Input placeholder='Department' />
+                            </Form.Item>
+                        </div>
+                    </div>
+                    <div className="leave_form-row">
+                        <div className="leave_item">
+                            <Form.Item
+                                label="Leave Duration"
+                                name="RangePicker"
+                                rules={[{ required: false, message: 'Please input!' }]}
+                            >
+                                <RangePicker />
+                            </Form.Item>
+                        </div>
+                    </div>
+                    <div className="leave_form-row">
+                        <div className="leave_item">
+                            <Form.Item name="Type" label="Select leave type" className='leavet'>
+                                <Select className="leave_Type" placeholder="Select leave type">
+                                    <Option value="General">General</Option>
+                                    <Option value="Specific">Annual</Option>
+                                    <Option value="Medical">Medical</Option>
+                                </Select>
+                            </Form.Item>
+                        </div>
+                    </div>
+                    <div className="leave_form-row">
+                        <div className="leave_item">
+                      
+                        </div>
+                    </div>
+                    <div className="leave_item">
+                        <Form.Item name="Description" label="Description">
+                            <Input.TextArea className='leave_Description' />
+                        </Form.Item>
+                    </div>
+                    <div className="leave_Button-cons">
+                    
+                        <Button className='leave_primary-button my-2' htmlType='submit'>Submit</Button>
+                    </div>
+                </Form>
+            </div>
+        </div>
+    )
+}
+
+export default LeaveEmpform;
