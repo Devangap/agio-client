1eb844ca3c2c0339ea8d57884e72ec15c6e8b8ab
shenal chanuka <125878126+shenalchanu8@users.noreply.github.com>
2024-04-04 19:46:06 +0530

    add908

--- src/pages/TraBooking.js
+++ src/pages/TraBooking.js
@@ -8,7 +8,7 @@ import { useNavigate } from 'react-router-dom';
 function TraBooking() {
 
     const navigate = useNavigate();
-
+ 
   const{Option} = Select;
 
   const onFinish = async(values) =>{
