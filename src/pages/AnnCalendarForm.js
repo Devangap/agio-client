36f644482b65023117dee7810a65aec63ab0b1c4
thinalfdo <thinalfernando1971@gmail.com>
2024-04-20 11:15:36 +0530

    a9

--- src/pages/AnnCalendarForm.js
+++ src/pages/AnnCalendarForm.js
@@ -1,80 +1,87 @@
-import React from 'react';
-import { Form, Input, DatePicker, Radio, Button } from 'antd';
+import React, { useState } from 'react';
+import { Form, Input, DatePicker, Radio, Button, Modal } from 'antd';
 import moment from 'moment';
 import "../leaveEmpform.css";
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import toast from 'react-hot-toast';
 
-function AnnCalendarForm() {
+function AnnCalendarForm({onFormSubmit}) {
   const [form] = Form.useForm();
   const navigate = useNavigate();
+  const [isModalVisible, setIsModalVisible] = useState(false); // State to control modal visibility
 
-  const onFinish = async(values) =>{
-    console.log('Recieved values of form', values);
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
 
+  const handleOk = () => {
+    setIsModalVisible(false);
+    form.submit(); // Submit form when OK is clicked on modal
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const onFinish = async (values) => {
     try {
-      const response = await axios.post('/api/employee/AnnCalNotice', values);
-      if(response.data.success){
-          toast.success(response.data.message);
-          navigate('/AnnCalendar');
-         
-          
-      }else{
-          toast.error(response.data.message);
-
-      }
-      
-  } catch (error) {
-      toast.error("Something went wrong");
-  }
-
-  }
+        const response = await axios.post('/api/employee/AnnCalNotice', values);
+        if (response.data.success) {
+            toast.success(response.data.message);
+            onFormSubmit(); // Closes the modal after successful submission
+        } else {
+            toast.error(response.data.message);
+        }
+    } catch (error) {
+        toast.error("Something went wrong");
+    }
+};
 
   return (
-    <Form
-      form={form}
-      onFinish={onFinish}
-      layout="vertical"
-      initialValues={{
-        confirmation: 'yes',
-        date: moment(), // Current date
-      }}
-    >
-      <Form.Item
-        label="Title"
-        name="title"
-        rules={[{ required: true, message: 'Please input your title!' }]}
-      >
-        <Input />
-      </Form.Item>
+    <>
+      <Button type="primary" onClick={showModal}>
+        Add Notice
+      </Button>
 
-      <Form.Item
-        label="Date"
-        name="date"
-        
-      >
-        <DatePicker
-          format="MMMM D, YYYY"
-          className="form-control"
-        />
-      </Form.Item>
+      <Modal title="Add Notice" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <Form
+          form={form}
+          onFinish={onFinish}
+          layout="vertical"
+          initialValues={{
+            confirmation: 'yes',
+            date: moment(), // Current date
+          }}
+        >
+          <Form.Item
+            label="Title"
+            name="title"
+            rules={[{ required: true, message: 'Please input your title!' }]}
+          >
+            <Input />
+          </Form.Item>
 
-      <Form.Item
-        label="Description"
-        name="description"
-      >
-        <Input.TextArea />
-      </Form.Item>
+          <Form.Item
+            label="Date"
+            name="date"
+          >
+            <DatePicker
+              format="MMMM D, YYYY"
+              className="form-control"
+            />
+          </Form.Item>
 
-      
+          <Form.Item
+            label="Description"
+            name="description"
+          >
+            <Input.TextArea />
+          </Form.Item>
 
-      <Form.Item>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+        </Form>
+      </Modal>
+    </>
   );
 }
 
